539e7531240308cc272a259bfbf5b8c187a24f4b
Nikita Pozdniakov <nikitosing4@mail.ru>
2025-03-12 17:47:40 +0300

    fix: Improve background migrations + new Indexer.Migrator.RecoveryWETHTokenTransfers

--- apps/explorer/lib/explorer/chain/cache/background_migrations.ex
+++ apps/explorer/lib/explorer/chain/cache/background_migrations.ex
@@ -53,7 +53,8 @@ defmodule Explorer.Chain.Cache.BackgroundMigrations do
     key: :heavy_indexes_create_arbitrum_batch_l2_blocks_unconfirmed_blocks_index_finished,
     key: :heavy_indexes_drop_transactions_created_contract_address_hash_with_pending_index_finished,
     key: :heavy_indexes_drop_transactions_from_address_hash_with_pending_index_finished,
-    key: :heavy_indexes_drop_transactions_to_address_hash_with_pending_index_finished
+    key: :heavy_indexes_drop_transactions_to_address_hash_with_pending_index_finished,
+    key: :heavy_indexes_create_logs_transaction_hash_block_hash_index_address_hash_index_finished
 
   @dialyzer :no_match
 
@@ -78,6 +79,7 @@ defmodule Explorer.Chain.Cache.BackgroundMigrations do
     CreateLogsAddressHashFirstTopicBlockNumberIndexIndex,
     CreateLogsBlockHashIndex,
     CreateSmartContractsLanguageIndex,
+    CreateLogsTransactionHashBlockHashIndexAddressHashIndex,
     DropInternalTransactionsFromAddressHashIndex,
     DropLogsAddressHashIndex,
     DropLogsAddressHashTransactionHashIndex,
@@ -281,6 +283,13 @@ defmodule Explorer.Chain.Cache.BackgroundMigrations do
     start_migration_status_task(
       DropTransactionsToAddressHashWithPendingIndex,
       &set_heavy_indexes_drop_transactions_to_address_hash_with_pending_index_finished/1
+    )
+  end
+
+  defp handle_fallback(:heavy_indexes_create_logs_transaction_hash_block_hash_index_address_hash_index_finished) do
+    start_migration_status_task(
+      CreateLogsTransactionHashBlockHashIndexAddressHashIndex,
+      &set_heavy_indexes_create_logs_transaction_hash_block_hash_index_address_hash_index_finished/1
     )
   end
 
